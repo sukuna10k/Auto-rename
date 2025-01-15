7300f621251be2bf046805302b3e3ccda9a0296a
sukuna10k <abdoulmadjidkingceymarkouma@gmail.com>
2025-01-15 06:14:09 +0000

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config(object):
 
     # database config
     DB_NAME = os.environ.get("DB_NAME","auto")     
-    DB_URL  = os.environ.get("DB_URL","mongodb+srv://<auto>:<autoren>@cluster0.mongodb.net/test?retryWrites=true&w=majority&tls=true")
+    DB_URL  = os.environ.get("DB_URL","mongodb+srv://<UKKi>:<flixking>@cluster0.mongodb.net/test?retryWrites=true&w=majority&tls=true")
  
     # other configs
     BOT_UPTIME  = time.time()
